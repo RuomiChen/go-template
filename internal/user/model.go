9d3c2d0a61b95357218a142e325b707dd85f1a7e
zihao.chen <zihao.chen@xilancloud.com>
2025-08-13 11:04:47 +0800

    feat:隐藏密码JSON展示

--- internal/user/model.go
+++ internal/user/model.go
@@ -5,7 +5,7 @@ import "mvc/internal/common"
 type User struct {
 	common.BaseModel
 	Username string `json:"username"`
-	Password string `json:"passwprd"`
+	Password string `json:"-"`
 	Name     string `json:"name"`
 	Email    string `gorm:"type:varchar(50);unique"`
 }

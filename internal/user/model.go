441b60e10528b961fdfd3ca168b1cc506645ea2a
zihao.chen <zihao.chen@xilancloud.com>
2025-08-13 16:41:15 +0800

    feat:用户字段添加头像

--- internal/user/model.go
+++ internal/user/model.go
@@ -6,6 +6,7 @@ type User struct {
 	common.BaseModel
 	Username string `json:"username"`
 	Password string `json:"-"`
+	Avatar   string `gorm:"type:varchar(150)" json:"avatar"`
 	Name     string `json:"name"`
 	Email    string `gorm:"type:varchar(50);unique"`
 }
